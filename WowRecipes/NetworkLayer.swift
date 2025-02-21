2e3f18e79553389a707ed674f9edf13cb8afe14d
zzz <bha.guang@gmail.com>
2025-02-21 13:03:56 -0500

    Add networkLayer

--- WowRecipes/NetworkLayer.swift
+++ WowRecipes/NetworkLayer.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+struct NetworkLayer {
+    let apiClient: APIClient
+
+    func requestData() async throws -> Data {
+        let request = URLRequest(url: try apiClient.constructToURL())
+        let (data, response) = try await URLSession.shared.data(for: request)
+
+        guard let res = response as? HTTPURLResponse else {
+            throw NetworkError.invalidResponse
+        }
+
+        guard res.statusCode < 400 else {
+            throw NetworkError.responseError(res.statusCode)
+        }
+
+        return data
+    }
+
+    func getRecipes(data: Data) throws -> [Recipe] {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+
+        let result = try decoder.decode(RecipesContainer.self, from: data)
+        let list = result.recipes
+        return list
+    }
+}
