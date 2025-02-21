08050080c546deb5cbedc708d2e5650a953437c3
zzz <bha.guang@gmail.com>
2025-02-20 23:53:00 -0500

    init new project

---
+++ WowRecipes/WowRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WowRecipesApp.swift
+//  WowRecipes
+//
+//  Created by func$ on 2/20/25.
+//
+
+import SwiftUI
+
+@main
+struct WowRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
